86c775cd31e60217a07fee65b567485c213bbfbd
Brian Hobbs <brianhobbs216@gmail.com>
2020-09-22 15:00:04 -0400

    added login, signup, and logout

--- pages/urls.py
+++ pages/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
 
     # AUTH
     path('register/', views.register, name="register"),
+    path('login/', views.login_user, name='login_user'),
+    path('logout/', views.logout_user, name='logout_user'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 
@@ -14,7 +14,7 @@ def index(request):
 
 def register(request):
     if request.method == 'GET':
-        return render(request, 'pages/signup.html', {'form':UserCreationForm()})
+        return render(request, 'pages/signup.html', {'form': UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
@@ -24,7 +24,26 @@ def register(request):
                 return redirect('main:index')
             except IntegrityError:
                 return render(request, 'pages/signup.html', {'form': UserCreationForm(),
-                                                                'error': 'That username has already been taken. Please choose a new username'})
+                                                             'error': 'That username has already been taken. Please choose a new username'})
         else:
             return render(request, 'pages/signup.html',
                           {'form': UserCreationForm(), 'error': 'Passwords did not match'})
+
+
+def login_user(request):
+    if request.method == 'GET':
+        return render(request, 'pages/login_user.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'pages/login_user.html',
+                          {'form': AuthenticationForm(), 'error': 'Username and password did not match'})
+        else:
+            login(request, user)
+            return redirect('main:index')
+
+
+def logout_user(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('main:index')
