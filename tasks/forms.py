9151aedff5ac4a416120b263305044a86c947a4f
Brian Hobbs <brianhobbs216@gmail.com>
2020-09-23 14:54:39 -0400

    done

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,8 +1,15 @@
 from django.forms import ModelForm
 from .models import Todo
+from django import forms
 
 
 class TodoForm(ModelForm):
     class Meta:
         model = Todo
         fields = ['title', 'memo', 'important', 'due_date']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'memo': forms.Textarea(attrs={'class': 'form-control'}),
+            'important': forms.CheckboxInput(attrs={'class': 'custom-control custom-checkbox'}),
+            'due_date': forms.TextInput(attrs={'class': 'form-control'}),
+        }
